43fd8e94fd497734ffc8bf4bf118ea62a143609e
Tori <mes.viktoria@mail.ru>
2022-09-10 23:49:17 +0500

    Cреднее арифметическое элементов в каждом столбце

---
+++ Task052/Program.cs
@@ -0,0 +1,44 @@
+﻿// Cреднее арифметическое элементов в каждом столбце.
+
+void FillArray(int[,] arr)
+{
+   for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1,10);
+        }
+    }    
+}
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] array = new int[3, 4];
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine();
+
+void ArithmeticMean(int[,] mas)
+{
+    for (int j = 0; j < mas.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < mas.GetLength(0); i++)
+        {
+            sum += mas[i,j];
+        }
+        sum = sum/mas.GetLength(0);
+        Console.Write($"{Math.Round(sum, 1)} ");
+    }
+
+}
+ArithmeticMean(array);
